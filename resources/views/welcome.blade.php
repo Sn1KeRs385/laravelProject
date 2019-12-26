1308b27ca41b4642c5230eb1ccd2eecafdc6d2e1
Sn1KeRs385 <Sn1KeRs385@gmail.com>
2019-12-26 18:10:05 +0500

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-TestTest
+                    Laravel-Test
                 </div>
 
                 <div class="links">
